15412a895ae4c1a7fd34ddf1b57d5fde6822feee
nikko187 <heavymachinery2@gmail.com>
2024-02-11 14:32:59 -0800

    cleaned code

--- Program.cs
+++ Program.cs
@@ -14,24 +14,6 @@ using Newtonsoft.Json;
 
 namespace IBKR_Rest_Sample
 {
-    public class Rootobject
-    {
-        public bool authenticated { get; set; }
-        public bool competing { get; set; }
-        public bool connected { get; set; }
-        public string message { get; set; }
-        public string MAC { get; set; }
-        public Serverinfo serverInfo { get; set; }
-        public string hardware_info { get; set; }
-        public string fail { get; set; }
-    }
-
-    public class Serverinfo
-    {
-        public string serverName { get; set; }
-        public string serverVersion { get; set; }
-    }
-
     class Program
     {
         public static Uri BaseUri = new Uri(baseURL);
